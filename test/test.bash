3f7478f2a084257837e608364f23d461f9f35261
Taka-bnbn <mizutaka2005@gmail.com>
2025-01-02 18:56:19 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -1,16 +1,27 @@
-name: test
-on: push
-jobs:
-  test:
-    runs-on: ubuntu-24.04
-    strategy:
-      matrix:
-         python-version: ["3.7","3.8","3.9","3.10","3.11"]
-      container: ryuichiueda/ubuntu22.04-ros2:latest
-    steps:
-      - uses: actions/checkout@v2
-      - name: build and test
-        run: |
-          rsync -av ./ /root/ros2_ws/src/mypkg/
-          cd /root/ros2_ws
-          bash -xv ./src/mypkg/test/test.bash /root
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Takaya Mizumaki 
+# SPDX-License-Identifier: BSD-3-Clause
+
+# ROS2ワークスペースの設定
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+# ROS2のワークスペースに移動してビルド
+cd $dir/ros2_ws || exit 1
+colcon build
+
+# ROS2環境変数をソース
+source $dir/.bashrc
+
+# ノードのテスト実行（10秒間）
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+# 現在の日本時間を取得（HHMMSS形式）
+timejp=$(TZ='Asia/Tokyo' date +%H%M%S)
+
+# 時間を2秒引いて検索用の時間を準備
+timejp=$((10#$timejp - 2))  # 10#でゼロ埋めの除去を防止
+
+# ログファイルから指定時間のメッセージを検索して表示
+cat /tmp/mypkg.log | grep "${timejp}"
+
