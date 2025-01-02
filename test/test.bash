82c18629a1b0727947858d4bb6594db49640a4db
Taka-bnbn <mizutaka2005@gmail.com>
2025-01-02 18:39:55 +0900

    wtf

--- test/test.bash
+++ test/test.bash
@@ -1,19 +1,16 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: 2024 Takaya Mizuzmaki
-# SPDX-License-Identifier: BSD-3-Clause
-
-dir=~
-[ "$1" != "" ] && dir="$1"
-
-cd $dir/ros2_ws
-colcon build
-
-source $dir/.bashrc
-
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-
-timejp=$(TZ='Asia/Tokyo' date +%H%M%S)
-timejp=$((timejp - 2))
-
-grep "${timejp}" /tmp/mypkg.log
-
+name: test
+on: push
+jobs:
+  test:
+    runs-on: ubuntu-24.04
+    strategy:
+      matrix:
+         python-version: ["3.7","3.8","3.9","3.10","3.11"]
+      container: ryuichiueda/ubuntu22.04-ros2:latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: build and test
+        run: |
+          rsync -av ./ /root/ros2_ws/src/mypkg/
+          cd /root/ros2_ws
+          bash -xv ./src/mypkg/test/test.bash /root
