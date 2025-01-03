92353dddaf1d8ec7249d0fb11c6899b5eb181687
Taka-bnbn <mizutaka2005@gmail.com>
2025-01-03 17:39:52 +0900

    test

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,12 +1,24 @@
+import launch
 from launch import LaunchDescription
+from launch.actions import LogInfo
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        LogInfo(
+            condition=launch.conditions.LaunchConfigurationEquals('verbose', 'true'),
+            msg="This is a test launch"
+        ),
         Node(
             package='mypkg',
-            executable='talker',
-            name='talker',
+            executable='talker',  # talker ノード名
+            name='talker_node',
+            output='screen'
+        ),
+        Node(
+            package='mypkg',
+            executable='listener',  # listener ノード名
+            name='listener_node',
             output='screen'
         ),
     ])
